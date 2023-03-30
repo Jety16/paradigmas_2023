844bd389736fa0c808e1e1c7166ead97bff42c67
Milagros Carabelos <mili.carabelos03@gmail.com>
2023-03-30 20:19:30 -0300

    rot45

--- Dibujos/Ejemplo.hs
+++ Dibujos/Ejemplo.hs
@@ -4,7 +4,7 @@ module Dibujos.Ejemplo
   )
 where
 
-import Dibujo (Dibujo, espejar, figura, r180, r270, rotar90)
+import Dibujo (Dibujo, espejar, figura, r180, r270, rot45, rotar90)
 import FloatingPic (Output, half, zero)
 import Graphics.Gloss (line, pictures, polygon, white)
 import qualified Graphics.Gloss.Data.Point.Arithmetic as V
@@ -13,20 +13,21 @@ import Interp (Conf (..), interp)
 type Basica = ()
 
 ejemplo :: Dibujo Basica
-ejemplo = r270 (figura ())
+ejemplo = figura ()
 
 --ejemplo = (rotar90 (rotar90 (rotar90 (figura ()))))
 
 interpBas :: Output Basica
-interpBas () a b c = pictures [cuadrado a b c, triangulo a b c]
+interpBas () a b c = pictures [cuadrado a b c]
   where
-    triangulo a b c = polygon $ map (a V.+) [zero, b V.+ c, b, zero]
-    cuadrado a b c = line [a, a V.+ c, a V.+ c V.+ b, zero, a]
+    triangulo a b c = polygon $ map (a V.+) [zero, c, b, zero]
+    --cara a b c = polygon $ triangulo (a V.+ half c) (half b) (half c)
+
+    cuadrado a b c = line [a , a V.+ c, a V.+ b V.+ c, a V.+ b, a]
 
 -- cara abc calls the polygon with the  vector list from triangulo a b c
 -- where a = (a V.+ half c) and b = (half b) and c = (half c)
 -- polygon is a data struc of type Picture
---cara a b c = polygon $ triangulo (a V.+ half c) (half b) (half c)
 
 ejemploConf :: Conf
 ejemploConf =
--- Interp.hs
+++ Interp.hs
@@ -1,22 +1,22 @@
 {-# OPTIONS_GHC -Wno-overlapping-patterns #-}
-module Interp (
-    interp,
-    Conf(..),
+
+module Interp
+  ( interp,
+    Conf (..),
     interpConf,
-    initial
-) where
+    initial,
+  )
+where
 
-import Graphics.Gloss(Picture, Display(InWindow), makeColorI, color, pictures, translate, white, display)
-import Dibujo (Dibujo(..), foldDib, figura, rotar90, rot45, r180, apilar, juntar, encimar, r270)
-import FloatingPic (FloatingPic, Output, grid, vacia, zero)
-
-import Graphics.Gloss.Data.Picture (Picture, rotate, blank)
-
+import Dibujo (Dibujo (..), apilar, encimar, figura, foldDib, juntar, r180, r270, rot45, rotar90)
+import FloatingPic (FloatingPic, Output, grid, half, vacia, zero)
+import Graphics.Gloss (Display (InWindow), Picture, color, display, makeColorI, pictures, translate, white)
+import Graphics.Gloss.Data.Picture (Picture, blank, rotate)
 import qualified Graphics.Gloss.Data.Point.Arithmetic as V
-
 import Graphics.Gloss.Data.Vector (Vector)
+import Language.Haskell.TH (Role)
 
--- Convert a FloatingPic to another FloatingPic. 
+-- Convert a FloatingPic to another FloatingPic.
 -- Basically take the vectors and modify it
 rotatePic :: FloatingPic -> FloatingPic
 rotatePic pic v1 v2 v3 = pic (v1 V.+ v2) (v3) (zero V.- v2)
@@ -24,6 +24,8 @@ rotatePic pic v1 v2 v3 = pic (v1 V.+ v2) (v3) (zero V.- v2)
 espejarPic :: FloatingPic -> FloatingPic
 espejarPic pic v1 v2 v3 = pic (v1 V.+ v2) (zero V.- v2) (v3)
 
+rot45Pic :: FloatingPic -> FloatingPic
+rot45Pic pic v1 v2 v3 = pic (v1 V.+ half (v2 V.+ v3)) (half (v2 V.+ v3)) (half (v3 V.- v2))
 
 -- IDEA DE MODIFICADOR DE VECTOR
 --espejarVector :: Vector -> Vector
@@ -37,31 +39,33 @@ interp f Vacia = vacia
 interp f (Figura a) = f a
 interp f (Rotar90 a) = rotatePic (interp f a)
 interp f (Espejar a) = espejarPic (interp f a)
+interp f (Rot45 a) = rot45Pic (interp f a)
 
 -- Configuración de la interpretación
-data Conf = Conf {
-        name :: String,
-        pic :: FloatingPic
-    }
+data Conf = Conf
+  { name :: String,
+    pic :: FloatingPic
+  }
 
 interpConf :: Conf -> Float -> Float -> Picture
-interpConf (Conf _ p) x y = p (0, 0) (x,0) (0,y)
+interpConf (Conf _ p) x y = p (0, 0) (x, 0) (0, y)
 
 -- Dada una computación que construye una configuración (AKA. Dibujos/*.hs), mostramos por
 -- pantalla la figura de la misma de acuerdo a la interpretación para
--- las figuras básicas. Permitimos una computación (que es una computacion?????) 
+-- las figuras básicas. Permitimos una computación (que es una computacion?????)
 -- para poder leer
 -- archivos, tomar argumentos, etc.
 
--- SOOO 
+-- SOOO
 -- el pictures toma una lista de dibujos para hacer, donde  el
 -- color grey $ grid (ceiling $ size / 10) (0, 0) x 10
 -- nos genera la grid basicamente
 
 initial :: Conf -> Float -> IO ()
 initial cfg size = do
-    let n = name cfg
-        win = InWindow n (ceiling size, ceiling size) (0, 0)
-    display win white $ withGrid (interpConf cfg size size) size size
-  where withGrid p x y = translate (-size/2) (-size/2) $ pictures [p, color grey $ grid (ceiling $ size / 10) (0, 0) x 10]
-        grey = makeColorI 120 120 120 120
+  let n = name cfg
+      win = InWindow n (ceiling size, ceiling size) (0, 0)
+  display win white $ withGrid (interpConf cfg size size) size size
+  where
+    withGrid p x y = translate (- size / 2) (- size / 2) $ pictures [p, color grey $ grid (ceiling $ size / 10) (0, 0) x 10]
+    grey = makeColorI 120 120 120 120
