ce0424d2368b534d33d654d2c89c3417fc132e17
Milagros Carabelos <mili.carabelos03@gmail.com>
2023-03-30 20:00:36 -0300

    cuadrado

--- Dibujos/Ejemplo.hs
+++ Dibujos/Ejemplo.hs
@@ -13,17 +13,20 @@ import Interp (Conf (..), interp)
 type Basica = ()
 
 ejemplo :: Dibujo Basica
-ejemplo = rotar90 (rotar90 (figura ()))
+ejemplo = r270 (figura ())
+
+--ejemplo = (rotar90 (rotar90 (rotar90 (figura ()))))
 
 interpBas :: Output Basica
-interpBas () a b c = pictures [cara a b c]
+interpBas () a b c = pictures [cuadrado a b c, triangulo a b c]
   where
-    triangulo a b c = map (a V.+) [zero, c, b, zero]
+    triangulo a b c = polygon $ map (a V.+) [zero, b V.+ c, b, zero]
+    cuadrado a b c = line [a, a V.+ c, a V.+ c V.+ b, zero, a]
 
-    -- cara abc calls the polygon with the  vector list from triangulo a b c
-    -- where a = (a V.+ half c) and b = (half b) and c = (half c)
-    -- polygon is a data struc of type Picture
-    cara a b c = polygon $ triangulo (a V.+ half c) (half b) (half c)
+-- cara abc calls the polygon with the  vector list from triangulo a b c
+-- where a = (a V.+ half c) and b = (half b) and c = (half c)
+-- polygon is a data struc of type Picture
+--cara a b c = polygon $ triangulo (a V.+ half c) (half b) (half c)
 
 ejemploConf :: Conf
 ejemploConf =
